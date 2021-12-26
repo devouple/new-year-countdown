d4ae07d35257d03dcf20c9004250b088a253d8d4
ChoJongHoon <chojonghoon@kakao.com>
2021-12-26 20:18:38 +0900

    [config] See you later React18

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLDivElement
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 )
-root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

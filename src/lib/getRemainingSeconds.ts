1020c4f6eaaf740d9db2d8d7eaecdb58c1142bfe
Luna Lee <htijl@naver.com>
2021-12-27 23:36:12 +0900

    [fix] extract logic

--- src/lib/getRemainingSeconds.ts
+++ src/lib/getRemainingSeconds.ts
@@ -1,7 +1,8 @@
 import getYear from 'date-fns/getYear'
 import differenceInSeconds from 'date-fns/differenceInSeconds'
 
+const nextYear = getYear(new Date()) + 1
+
 export const getRemainingSeconds = () => {
-	const nextYear = getYear(new Date()) + 1
 	return differenceInSeconds(new Date(nextYear, 0, 1), new Date())
 }

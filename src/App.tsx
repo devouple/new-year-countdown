1b2ca56d1d8ffea98ccb7ee040b810e70d7ef8ac
Luna Lee <htijl@naver.com>
2021-12-27 22:59:10 +0900

    [feat] add timeover logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import { useEffect, useState } from 'react'
+import getMonth from 'date-fns/getMonth'
+import { useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
 import { Digit } from './components/Digit'
+import { Fireworks } from './components/Fireworks'
 import { getRemainingSeconds } from './lib/getRemainingSeconds'
 
 function App() {
 	const [state, setState] = useState(getRemainingSeconds())
+	const [isDone, setIsDone] = useState(getMonth(new Date()) === 1)
+	const ref = useRef<NodeJS.Timer | null>(null)
 
 	useEffect(() => {
-		setInterval(() => {
+		ref.current = setInterval(() => {
+			const remainingSeconds = getRemainingSeconds()
+			if (remainingSeconds < 1) {
+				setIsDone(true)
+			}
 			setState(getRemainingSeconds())
 		}, 100)
 	}, [])
 
+	useEffect(() => {
+		if (isDone && ref.current) {
+			clearInterval(ref.current)
+		}
+	}, [isDone])
+
 	return (
 		<div>
-			<Container>
-				{state
-					.toString()
-					.split('')
+			{isDone ? (
+				<Fireworks />
+			) : (
+				<Container>
+					{state
+						.toString()
+						.split('')
 						.map((digit, idx, arr) => (
 							<Digit key={arr.length - idx} digit={digit} />
-					))}
-			</Container>
+						))}
+				</Container>
+			)}
 		</div>
 	)
 }
